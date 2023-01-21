6a2de1c963d7b8b09123344d114d8dafb81e0ba3
Kim Nørgaard <kn@netic.dk>
2023-01-21 22:14:49 +0100

    fix another count bug

--- main.tf
+++ main.tf
@@ -59,9 +59,9 @@ resource "aws_s3_bucket" "terraform_state" {
 }
 
 resource "aws_s3_bucket_logging" "terraform_state" {
-  for_each = var.bucket_logging
+  for_each = var.bootstrap > 0 ? var.bucket_logging : {}
 
-  bucket = aws_s3_bucket.terraform_state.id
+  bucket = aws_s3_bucket.terraform_state[0].id
   target_bucket = each.value.target_bucket
 }
 
